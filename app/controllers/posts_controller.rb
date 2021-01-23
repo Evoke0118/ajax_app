db823794525438feef062874999f71eb16e3b3a1
Evoke0118 <evoke.baddvoltage@gmail.com>
2021-01-23 18:41:20 +0900

    AjaxAppの準備

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,13 @@
+class PostsController < ApplicationController
+
+  def index  # indexアクションを定義した
+    @posts = Post.all.order(id: "DESC")  # 新しいメモが一番上に表示されるように
+  end
+
+
+  def create
+    Post.create(content: params[:content])
+    redirect_to action: :index   #メモを保存した後にトップページへリダイレクトされるように。
+  end
+
+end
